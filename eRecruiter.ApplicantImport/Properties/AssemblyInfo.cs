05f275b55269121a949203894ac009261994a1af
Hannes Sachsenhofer <hannes@sachsenhofer.com>
2014-09-19 14:32:26 +0200

    Added AssemblyInfo (for build versioning).

---
+++ eRecruiter.ApplicantImport/Properties/AssemblyInfo.cs
@@ -0,0 +1,13 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyTitle("eRecruiter.ApplicantImport")]
+[assembly: AssemblyDescription("Tool to import applicant data from CSV into eRecruiter.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Hannes Sachsenhofer")]
+[assembly: AssemblyProduct("")]
+[assembly: AssemblyCopyright("")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: AssemblyVersion("0.0.0.0")]
+[assembly: AssemblyFileVersion("0.0.0.0")]
